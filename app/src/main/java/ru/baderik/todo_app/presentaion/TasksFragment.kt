c8353c201722c0c08760657940581e6007dc60d3
Renard <renard.mbm@yandex.ru>
2023-06-11 05:15:21 +1000

    Initial recycler view in TasksFragment

--- app/src/main/java/ru/baderik/todo_app/presentaion/TasksFragment.kt
+++ app/src/main/java/ru/baderik/todo_app/presentaion/TasksFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
 import ru.baderik.todo_app.databinding.FragmentTasksBinding
 
 class TasksFragment : Fragment() {
 
     private lateinit var binding: FragmentTasksBinding
+    private val adapter = ColumnAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,7 +21,17 @@ class TasksFragment : Fragment() {
     ): View {
         binding = FragmentTasksBinding.inflate(inflater, container, false)
 
+        initRecyclerView()
+
         return binding.root
+    }
+
+    private fun initRecyclerView() {
+        val layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        val snapHelper = PagerSnapHelper()
+        binding.columnRecyclerView.layoutManager = layoutManager
+        binding.columnRecyclerView.adapter = adapter
+        snapHelper.attachToRecyclerView(binding.columnRecyclerView)
     }
 
     companion object {
