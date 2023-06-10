1da1f5f9dfa2e8a4a5f57037adaabf3b3e18b450
Renard <renard.mbm@yandex.ru>
2023-06-11 02:58:32 +1000

    Add fragment holder to MainActivity and TasksFragment basis

--- app/src/main/java/ru/baderik/todo_app/MainActivity.kt
+++ app/src/main/java/ru/baderik/todo_app/MainActivity.kt
@@ -1,11 +1,23 @@
 package ru.baderik.todo_app
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.baderik.todo_app.databinding.ActivityMainBinding
+import ru.baderik.todo_app.presentaion.TasksFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add( R.id.fragmentHolder, TasksFragment.newInstance())
+                .commit()
+        }
     }
 }
