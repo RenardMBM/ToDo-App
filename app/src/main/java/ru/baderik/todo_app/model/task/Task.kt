c1dbae1351c8bd6b21f06d90e0eb506ff60c2a9c
Renard <renard.mbm@yandex.ru>
2023-06-11 07:47:19 +1000

    Change task model with room annotations

--- app/src/main/java/ru/baderik/todo_app/model/task/Task.kt
+++ app/src/main/java/ru/baderik/todo_app/model/task/Task.kt
@@ -1,6 +1,12 @@
 package ru.baderik.todo_app.model.task
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity
 data class Task(
+    @PrimaryKey val id: UUID = UUID.randomUUID(),
     var title: String,
     var additionalInfo: String,
     var isCompleted: Boolean = false,
