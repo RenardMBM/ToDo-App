7691791c08b39000bcd0a5de6147e691ec372b5a
Renard <renard.mbm@yandex.ru>
2023-06-11 18:16:17 +1000

    Add task repository in TasksViewModel

--- app/src/main/java/ru/baderik/todo_app/presentaion/tasks/TasksViewModel.kt
+++ app/src/main/java/ru/baderik/todo_app/presentaion/tasks/TasksViewModel.kt
@@ -1,20 +1,22 @@
 package ru.baderik.todo_app.presentaion.tasks
 
 import android.util.Log
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import ru.baderik.todo_app.model.task.Task
+import ru.baderik.todo_app.model.task.TaskRepository
 
 class TasksViewModel : ViewModel() {
 
-    private val allTasks = MutableLiveData<List<Task>>()
-    private val favoriteTasks = MutableLiveData<List<Task>>()
-    private val completedTasks = MutableLiveData<List<Task>>()
+    private val taskRepository = TaskRepository.get()
+    private val allTasks: LiveData<List<Task>> = taskRepository.getAllTasks()
+    private val favoriteTasks: LiveData<List<Task>> = taskRepository.getFavouriteTasks()
+    private val completedTasks: LiveData<List<Task>> = taskRepository.getCompletedTasks()
 
     fun createTask(task: Task) = viewModelScope.launch {
-        Log.d("tasks", "Tasks created\n$task")
+        taskRepository.addTask(task)
     }
 
 }
